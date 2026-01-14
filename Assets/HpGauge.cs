7f1089d77a493de586c83a950d8de3a1453e6b21
takumimori0813-lang <takumi.mori0813@gmail.com>
2026-01-14 11:36:34 +0900

    G5_game2

--- Assets/HpGauge.cs
+++ Assets/HpGauge.cs
@@ -5,17 +5,16 @@ using UnityEngine.UI;
 public class HpGauge : MonoBehaviour
 {
     public int HP = 100;
-    private Func<Rigidbody2D> myRigidbody;
+    public Slider hpSlider;
     private int maxHP;
     public Image HPBar;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        myRigidbody = this.GetComponent<Rigidbody2D>;
         maxHP = HP;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Swwets")
         {
